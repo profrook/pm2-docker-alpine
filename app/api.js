00ec3b12b8afe587e54f8cb5131bca32ff9855b2
Krzysztof Gawrys <kgawrys@concisesoftware.com>
2016-12-13 11:39:56 +0100

    hello mofo

--- app/api.js
+++ app/api.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('hey');
+  res.end('hello world!');
 }).listen(process.env.PORT || 3000, function() {
   console.log('App listening on port 3000');
 });
